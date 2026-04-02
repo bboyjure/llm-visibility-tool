b8623292848b0b16df511d35afc389facc0b4f7f
Jure Beton <49736249+bboyjure@users.noreply.github.com>
2026-04-02 16:26:55 +0200

    Fix for forbidden searxng

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,11 @@ export default defineConfig({
         target: 'http://localhost:8888',
         rewrite: (path) => path.replace(/^\/api\/searxng/, ''),
         changeOrigin: true,
+        headers: {
+          'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+          'Accept-Language': 'en-US,en;q=0.5',
+          'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:120.0) Gecko/20100101 Firefox/120.0',
+        },
       },
     },
   },
